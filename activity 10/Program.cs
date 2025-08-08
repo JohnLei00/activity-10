a58f56fc5f6236380b1864f691b5d1fc3a43b95c
Admin <Admin@ELLA>
2025-08-09 06:03:22 +0800

    Add project files.

---
+++ activity 10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < 12; i++)
+        {
+            Console.Write("Enter number: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        List<int> evenList = new List<int>();
+        List<int> oddList = new List<int>();
+
+        foreach (var num in numbers)
+        {
+            if (num % 2 == 0) evenList.Add(num);
+            else oddList.Add(num);
+        }
+
+        Console.WriteLine("Even Numbers: " + string.Join(", ", evenList));
+        Console.WriteLine("Odd Numbers: " + string.Join(", ", oddList));
+    
+    }
+}
